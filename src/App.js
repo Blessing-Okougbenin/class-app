218cb02f5960e979379ff2c136fc0a6b230becea
Blessing Okougbenin <blessingokougbenin879@gmail.com>
2022-10-14 16:47:02 +0100

    My first react work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CardDetail from "./components/CardDetail";
+// import Nav from "./components/Nav";
+import SpacingFlex from "./components/SpacingFlex";
+import { faker } from '@faker-js/faker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function greet(){
+  return  " Hello from greet"
 }
 
+const App = () => {
+
+  const style = {color: "blue"};
+  const title = "Hello there";
+  const title2 = {name: "This", quality: "astonishingly beautiful"}
+
+  return (
+    <div>
+      <h1>{greet()}</h1>
+      <h2 style={style}>{title2.name} is {title2.quality}</h2>
+      {/* <Nav/> */}
+      <CardDetail image={faker.image.nature()}/>
+      <CardDetail image={faker.image.animals()}/>
+      <CardDetail image={faker.image.food()}/>
+
+      <SpacingFlex/>
+      <br></br>
+      <hr></hr>
+      <SpacingFlex/>
+
+
+    </div>
+  
+  )
+};
 export default App;
+
+
+// babel does the transpilation of this code
+// this is a declarative code
+// you can nest a component inside a component
+
+/*
+
+    <div>
+      <h1 className="main-text">Hello Fellas, this is react</h1>
+      <h2>It's great to be finally here!!!</h2>
+      <label for="name">Name: </label>
+      <input id="name" placeholder="enter your name" />
+      <button style={{ backgroundColor: "blue", color: "white" }} type="submit">submit</button>
+          </div>
+  );
+*/
---
+++ src/components/SpacingFlex.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SpacingFlex = () => {
+  return (
+    <div className="px-2">
+  <div className="flex -mx-2">
+    <div className="w-1/4 px-1">
+      <div className="bg-blue-400 h-6"></div>
+    </div>
+    <div className="w-1/4 px-1">
+      <div className="bg-blue-500 h-6"></div>
+    </div>
+    <div className="w-1/4 px-1">
+      <div className="bg-blue-400 h-6"></div>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default SpacingFlex
